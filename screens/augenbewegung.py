3e3eb33b92112d6beff0ca8447813cdd9f1c09fa
Hamza Ahmed <hamza.ahmed@stud.uni-due.de>
2026-06-05 12:18:58 +0200

    add: screen für augenbewegung vorbereitet

--- screens/augenbewegung.py
+++ screens/augenbewegung.py
@@ -5,15 +5,46 @@ class AugenBewegung(tk.Frame):
     def __init__(self, parent, app):
         super().__init__(parent)
 
-        tk.Label(self, text="Augenbewegungen", font=("Arial", 24)).pack(pady=20)
+        main_frame = tk.Frame(self)
+        main_frame.pack(anchor="center")
 
         tk.Label(
-            self,
-            text="Hier kommt später die Augenbewegungsaufgabe hin."
+            main_frame,
+            text="Augenbewegungen",
+            font=("Arial", 24)
         ).pack(pady=20)
 
+        self.canvas = tk.Canvas(
+            main_frame,
+            width=1400,
+            height=700,
+            bg="white"
+        )
+        self.canvas.pack()
+
+        controls_frame = tk.Frame(main_frame)
+        controls_frame.pack(pady=20)
+
+        button_frame = tk.Frame(controls_frame)
+        button_frame.pack()
+
         tk.Button(
-            self,
+            button_frame,
+            text="Sakkaden"
+        ).pack(side="left", padx=20)
+
+        tk.Button(
+            button_frame,
+            text="Vergenzbewegung"
+        ).pack(side="left", padx=20)
+
+        tk.Button(
+            button_frame,
+            text="Smooth Pursuit"
+        ).pack(side="left", padx=20)
+
+        tk.Button(
+            controls_frame,
             text="Zurück zum Menü",
             command=app.show_main_menu
-        ).pack(pady=20)
+        ).pack(pady=15)
